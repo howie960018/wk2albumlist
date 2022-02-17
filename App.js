8ebf3188479e7bae18a288e80d7c770a6f99c07f
howie960018 <84056899+howie960018@users.noreply.github.com>
2022-02-18 00:41:18 +0800

    Hello RN

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.bigwhite}>Hello RN </Text>
+      <Image  style={{
+            resizeMode: "stretch",
+            height: 300,
+            width: 200
+          }} source={require('./assets/pixiv.jpg')}
+      />
+      <Text style={styles.bigwhite}>110819005 數位二甲 曾浩儀 </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +19,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,    
+    backgroundColor: '#8BEDFE',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  bigwhite: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
   },
 });
